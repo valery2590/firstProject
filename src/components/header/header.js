17a5fc15ac02b15fd5ce3dfacb4c5c879e5d0a1e
Fernando Delgado Casariego <fernandodelgadocasariego@DESKTOP-RK84NV0.localdomain>
2020-12-16 17:39:09 +0100

    fernando

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,29 @@
 import './header.css';
 import Logo from '../../assets/logo.png';
+import { useHistory } from "react-router";
+import HomePage from "../../pages/homePage";
 
-const Header = () => {
+
+const Header = ({ user }) => {
+    const history = useHistory()
     return (
-    <div className="header__container">
-        <img src = {Logo} alt="Picturest Logo" />
-    </div>
-    
-    )}
+        <div className="header__container">
+            <div className="header__logo">
+                <img src={Logo} alt="Picturest Logo" />
+                <div className="button__container" onClick={() => history.push("/")}>
+                    Home
+            </div>
+            </div>
+            <div className="header_userInfo__container">
+                <img
+                    src={user.avatar}
+                    className="header_userInfo__image"
+                    alt="User avatar"
+                />
+            </div>
+        </div>
+    );
+};
 
-
-export default Header; 
+export default Header;
 
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,6 +1,6 @@
 import "../App.css";
 import BoardList from "../components/boardList/boardList";
-import PinsList from "../components/pinList/pinList";
+import PinsList from "../components/pinList/PinsList";
 import UserCard from "../components/userCard/userCard";
 
 function HomePage({user}) {
