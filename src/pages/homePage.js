edcd5347cf7444daa9916e3b2a0c59d9d7715722
Valery <valeryfigueroah@gmail.com>
2021-02-09 01:34:54 +0100

    new changes

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,16 +1,31 @@
 import "../App.css";
 import BoardList from "../components/boardList/boardList";
-import UserCard from "../components/userCard/userCard";
 import PinsList from "../components/pinList/PinsList";
+import Axios from "axios"
+import { useState } from "react";
 
 
-function HomePage({ user }) {
+function HomePage() {
+
+const [image, setImage] = useState("");
+  const images = ()=>{
+    Axios.get(`https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q`)
+    .then ((response) => {console.log(response);
+    setImage(response.data.hits)
+    }
+    
+    
+    )
+  }
+ 
   return (
     <div className="app__body">
-     
+   
       <div>
         <p>Welcome to Picturest</p>
+        {images}
       </div>
+      <button onClick={images}>Click</button>
       <BoardList />
       <PinsList/>
     </div>
