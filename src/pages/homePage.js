3ebcca1c4cf535dcdbb2803edd59b3127f4a51a6
Valery <valeryfigueroah@gmail.com>
2021-02-14 00:39:05 +0100

    search bar added

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,59 +1,66 @@
 import "../App.css";
 import BoardList from "../components/boardList/boardList";
 import PinsList from "../components/pinList/PinsList";
-import  { React, useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 
 function HomePage() {
-  const [formData, setFormData] = useState("Escribe tu busqueda")
-  const [respuesta, setRespuesta] = useState({results:[]});
-  const [res , setRes] = useState({results:[]})
-  const [response, setResponse] = useState({hits:[]});
-  
+  const [formData, setFormData] = useState("");
 
+  const [search, setSearch] = useState();
+  const [response, setResponse] = useState({ hits: [] });
+  const [isError, setIsError] = useState(false)
 
-  const busqueda = (e)=>{
-    setFormData(e.target.value)
-  }
+  const busqueda = (e) => {
+    setFormData(e.target.value);
+  };
 
+  // API Rick & Morty
+  // const [res , setRes] = useState({results:[]})
+  // useEffect ( () =>{
+  // const fetchData = async () => {
+  //   const res =  await fetch (`https://rickandmortyapi.com/api/character`);
+  //   const data2 = await res.json();
+  //   console.log(data2);
+  //   setRes(data2);
+  // }
+  // fetchData();
+  // }, []);
 
-  useEffect(async () => {
-    const response = await fetch(`https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=  &per_page=200`);
-    const data = await response.json();
-    console.log(data);
-    setResponse(data);
-  }, []); 
-console.log(response);
-
-
-
-useEffect ( async () => {
-  const res =  await fetch (`https://rickandmortyapi.com/api/character`);
-  const data2 = await res.json();
-  console.log(data2);
-  setRes(data2)
-}, []);
+  useEffect(() => {
+    const getpicture = async () => {
+      setIsError(false)
+      try{
+      const response = await fetch(
+        `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${formData}&per_page=200`
+      );
+      const data = await response.json();
+      setResponse(data);
+    }
+    catch (error) {
+      setIsError(true)
+    }
+    };
+    getpicture();
+  }, [search]);
 
   return (
     <div className="app__body">
-      <p>{formData}</p>
-      <input placeholder="select your option" onChange={busqueda}   />
-      <div>
-        {setRespuesta}
-      </div>
-      <div>
-        {response.hits.map(datos => {
-          return <img  key={datos.id} src={datos.previewURL} />;
-        })}
-      </div>
       <p>Welcome to Picturest 😄</p>
+      <p>{formData}</p>
+      <input
+        type="text"
+        placeholder="select your option"
+        value={formData}
+        onChange={busqueda}
+      />
+      <button onClick={() => setSearch(formData)}>Search</button>
+      {isError && <div>Something went wrong...</div>}
       <div>
-        {res.results.map(datos2 =>{
-          return <img key ={datos2.id} src={datos2.image}/>;
+        {response.hits.map((datos) => {
+          return <img key={datos.id} src={datos.previewURL} />;
         })}
       </div>
-      <button>Click</button>
-      <BoardList />
-      <PinsList />
+
     </div>
   );
 }
--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,8 +1,35 @@
 import LoginForm from "../components/loginForm/loginForm";
+import {useState, useEffect} from 'react';
+
 
 const LoginPage = () => {
+
+    const [res, setRes] = useState({results:[]})
+
+    useEffect(()=> {
+        const getRickMorty = async ()=>{
+            const response = await fetch (`https://rickandmortyapi.com/api/character`)
+            const data = await response.json();
+            console.log(data)
+            setRes(data);
+        };
+        getRickMorty();
+    }, [])
+
     return (
         <div>
+            <div>
+            {res.results.map((datos) => {
+                return <p key ={datos.id}> {datos.name}</p>})}
+                </div>
+                <div>
+            {res.results.map((datos) => {
+                return <img key ={datos.id} src={datos.image} />})}
+                </div>
+                <div>
+            {res.results.map((datos) => {
+                return <p key ={datos.id}> {datos.origin.name}</p>})}
+                </div>
         <LoginForm/>
         </div>
     )
